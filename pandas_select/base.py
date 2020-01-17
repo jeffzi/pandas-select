2e9438ac78a297512b772f096cf7e93d1e4ed4e3
jeffzi <jzinque@gmail.com>
2020-01-17 21:15:49 +0100

    Add Selector.__str__

--- pandas_select/base.py
+++ pandas_select/base.py
@@ -17,10 +17,27 @@ class Selector(ABC):
     def __call__(self, df: pd.DataFrame) -> IndexerValues:
         return self.select(df)
 
+    def _format(self, args=None):
+        pretty_cls = type(self).__name__
+        pretty_args = f"({', '.join(args)})" if args else ""
+        return pretty_cls + pretty_args
+
     def __repr__(self):
         args = [
             f"{param}={repr(vars(self)[param])}"
             for param in inspect.signature(self.__init__).parameters
             if param in vars(self)  # param is a class attribute
         ]
-        return f"{type(self).__name__}({', '.join(args)})"
+        return self._format(args)
+
+    def __str__(self):
+        args = []
+        for param_name, param in inspect.signature(self.__init__).parameters.items():
+            try:
+                value = vars(self)[param_name]
+                if param.default != value:
+                    args.append(f"{param_name}={str(value)}")
+            except KeyError:
+                pass  # param is not a class attribute
+
+        return self._format(args)
