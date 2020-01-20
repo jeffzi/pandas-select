cda9acb93099a88cff58f0bb62e91b548cfa7c32
jeffzi <jzinque@gmail.com>
2020-01-20 10:16:46 +0100

    Fix to_list type hints

--- pandas_select/utils.py
+++ pandas_select/utils.py
@@ -1,10 +1,13 @@
-from collections.abc import Iterable
+import typing
+
+from collections.abc import Sequence
+from typing import Any, Union
 
 
-def is_seq(x):
-    """Return True if x is iterable, except if x is a string"""
-    return isinstance(x, Iterable) and not isinstance(x, str)
+def is_seq(x: Any) -> bool:
+    """ Return True if x is a Sequence, except if x is a string """
+    return isinstance(x, Sequence) and not isinstance(x, str)
 
 
-def to_list(x):
+def to_list(x: Union[Any, typing.Sequence[Any]]) -> typing.Sequence[Any]:
     return x if is_seq(x) else [x]
