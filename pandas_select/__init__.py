bf7218a236105f9370b7e24833cec20055302314
jeffzi <jzinque@gmail.com>
2020-01-23 11:12:36 +0100

    Provide direct import for selectors

--- pandas_select/__init__.py
+++ pandas_select/__init__.py
@@ -1,5 +1,22 @@
-from .index import OneOf
+from .column import AllNominal, AllNumeric, HasDtype
+from .index import Contains, EndsWith, Everything, Exact, Match, OneOf, StartsWith
 from .where import Anywhere, Everywhere
 
 
-__all__ = ["OneOf", "Anywhere", "Everywhere"]
+__all__ = [
+    # column
+    "AllNominal",
+    "AllNumeric",
+    "HasDtype",
+    # index
+    "Contains",
+    "EndsWith",
+    "Everything",
+    "Exact",
+    "Match",
+    "OneOf",
+    "StartsWith",
+    # where
+    "Anywhere",
+    "Everywhere",
+]
