577a3c0b98afe2dae80d169c45eade9e7a8babaa
jeffzi <jzinque@gmail.com>
2020-01-27 13:09:15 +0100

    Bump to version 0.1.2

--- pandas_select/__init__.py
+++ pandas_select/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .column import AllNominal, AllNumeric, HasDtype
 from .index import Contains, EndsWith, Everything, Exact, Match, OneOf, StartsWith
