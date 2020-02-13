aa0d0d4622baa04c56f77a83742e7d900a37ef38
jeffzi <jzinque@gmail.com>
2020-02-13 15:14:29 +0100

    Add exclude arg to HasDtype

--- pandas_select/column.py
+++ pandas_select/column.py
@@ -5,13 +5,16 @@ from .index import Indexer
 
 
 class HasDtype(Indexer):
-    def __init__(self, dtypes) -> None:  # type: ignore
+    def __init__(
+        self, include: Optional[Dtypes] = None, exclude: Optional[Dtypes] = None
+    ) -> None:
         super().__init__(axis="columns", level=None)
-        self.dtypes = to_set(dtypes)
+        self.include = include and to_set(include)
+        self.exclude = exclude and to_set(exclude)
 
     def select(self, df: pd.DataFrame) -> pd.Index:
         df_row = df.iloc[:1]
-        return df_row.select_dtypes(self.dtypes).columns
+        return df_row.select_dtypes(self.include, self.exclude).columns
 
 
 class AllNumeric(HasDtype):
