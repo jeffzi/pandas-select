6e870ec8319ab7a37cbd365fddad622e4235bd1a
Jean-Francois Zinque <jzinque@gmail.com>
2020-12-23 16:36:23 +0100

    :wrench: Update documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -33,14 +33,14 @@ release = pandas_select.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinxcontrib.contentui",
+    "IPython.sphinxext.ipython_directive",
+    "sphinx_copybutton",
+    "sphinx_panels",
     "sphinx.ext.autodoc",
-    "sphinx.ext.autodoc.typehints",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
     "sphinx.ext.linkcode",
     "sphinx.ext.napoleon",
-    "IPython.sphinxext.ipython_directive",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,16 +56,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
-html_theme_options = {
-    "font_family": '"Avenir Next", Calibri, "PT Sans", sans-serif',
-    "head_font_family": '"Avenir Next", Calibri, "PT Sans", sans-serif',
-    "font_size": "16px",
-    "page_width": "1140px",
-    "github_user": "jeffzi",
-    "github_repo": "pandas-select",
-    "github_banner": True,
-}
+pygments_style = "friendly"
+html_theme = "furo"
+html_baseurl = "https://pandas-select.readthedocs.io/en/latest/"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -108,7 +101,12 @@ intersphinx_mapping = {
     "numpy": ("https://docs.scipy.org/doc/numpy/", None),
     "pandas": ("http://pandas.pydata.org/pandas-docs/stable/", None),
     "sklearn": ("http://scikit-learn.org/stable", None),
+    "pandera": ("https://pandera.readthedocs.io/en/stable", None),
 }
 
 autosummary_generate = True
+autodoc_typehints = "none"
 add_module_names = False
+
+copybutton_prompt_text = r">>> |\.\.\.\: |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
